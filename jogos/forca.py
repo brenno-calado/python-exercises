4027c8332930004fe88dfc9f520bc432900f3ef1
brenno-calado <brenno.calado@gmail.com>
2021-01-22 10:32:22 -0300

    gameMode enabled and building coloring text with ANSI

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,26 +1,47 @@
 def jogar():
+    from getpass import getpass
+    from colorama import Fore
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
-    print("*********************************")
-
-    secret_word = "banana"
-    acertos = len(secret_word) * ['_']
-    enforcou = False
-    acertou = False
-    print(acertos)
-    while(not enforcou or not acertou):
-        falta = acertos.count('_')
-        print('Tentativa {} de {}...')
-        print('Faltam {} letras para acertar a palavra'.format(falta))
+    print("*********************************"'\n\n')
+    secret_word = ''
+    while(secret_word == ''):
+        game_mode = input('Digite o modo do jogo: (1) jogador OU (2) jogadores: ')
+        if(game_mode == '2'):
+            secret_word = getpass("coloque uma palavra secreta para o amigo adivinhar: ").upper()
+        elif(game_mode == '1'):
+            secret_word = 'jazz'.upper()
+        else:
+            print('Por favor, Digite o número 1 ou 2...')
+    acertos = len(secret_word) * [' _ ']
+    tentativa = 0
+    chances = 6
+    print('palavra: ',''.join(acertos),'\n')
+    while(True):
+        print('Tentativa {} de {}...'.format(tentativa + 1,chances))
+        print('Faltam {} letras para acertar a palavra'.format(acertos.count(' _ ')))
         chute = input('Digite uma letra:')
-        chute = chute.strip()
-        index = 0
-        for letra in secret_word:
-            if(chute.upper() == letra.upper()):
-                acertos[index] = letra
-            index += 1
+        chute = chute.strip().upper()
+        print('*******************************************************')
+        if(chute in secret_word):
+            index = 0
+            for letra in secret_word:
+                if(chute == letra.upper()):
+                    acertos[index] = " {} ".format(letra)
+                index += 1
+            print('\033[31m','Acertou!')
+            print('\033[39m')
+        else:
+            tentativa += 1
+            print('\033[31m','Ops! Você errou!')
+            print('\033[39m')
         print('palavra: ',''.join(acertos),'\n')
-        
+        if(acertos.count(' _ ') == 0):
+            print('Parabéns!')
+            break
+        elif(tentativa == chances):
+            print('Tente outra vez!')
+            break
 
     print("Fim do jogo")
 
